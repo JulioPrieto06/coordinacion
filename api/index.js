3407a650ef9622144c3a94d4e1fda6a423199652
Ing. Prieto <ing.prieto@Mac-mini-de-UM.local>
2024-07-24 17:29:19 -0600

    removimos port:3000 de las env

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const serverless = require('serverless-http');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // Middleware
 app.use(cors());
